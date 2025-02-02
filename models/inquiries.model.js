d38ac3b2ab1518a7776456456aa11a1531972baa
TharHtet236401 <tharhteta3@gmail.com>
2025-02-03 01:17:24 +0700

    develop inquiries model

---
+++ models/inquiries.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const inquirySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email address']
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  companyName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  country: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  jobTitle: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  jobDetails: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Inquiry = mongoose.model("Inquiry", inquirySchema);
+
+export default Inquiry;
+
